4c67ec998f98509dcc6ec049c5a1517580d84691
leandrocodes <leaxviana140@gmail.com>
2019-11-28 09:43:39 -0400

    testing register almost done

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,14 +4,14 @@
       <div class="columns is-centered is-mobile">
         <div class="column is-4-fullhd is-4-desktop is-6-widescreen is-6-tablet is-11-mobile">
           <h1 class="has-text-centered has-text-light title">Registro</h1>
-          <form class="form-login">
+          <div class="form-login">
             <figure class="image">
               <img src="../assets/img/login-illustration.png" alt srcset />
             </figure>
             <div class="field">
               <label class="label has-text-dark has-text-weight-semibold is-size-6">Email</label>
               <div class="control has-icons-left has-icons-right">
-                <input class="has-text-dark input" type="email" placeholder="nome@mail.com" />
+                <input class="has-text-dark input" type="email" placeholder="nome@mail.com" v-model="email" />
                 <span class="icon is-small is-left">
                   <i class="fas fa-user"></i>
                 </span>
@@ -20,7 +20,8 @@
             <div class="field">
               <label class="label has-text-dark has-text-weight-semibold is-size-6">Senha</label>
               <div class="control has-icons-left has-icons-right">
-                <input class="has-text-dark input" type="password" placeholder="***" />
+                <input class="has-text-dark input" type="password" placeholder="***" v-model="password"/>
+                <p>{{ password }}</p>
                 <span class="icon is-small is-left">
                   <i class="fas fa-lock"></i>
                 </span>
@@ -28,14 +29,14 @@
             </div>
             <div class="field is-grouped">
               <div class="control">
-                <button @click.prevent="register" class="button is-link">Criar conta</button>
+                <button @click="register" class="button is-link">Criar conta</button>
               </div>
             </div>
             <div class="field">
               Já tem uma conta?
               <a class="has-text-info">Login</a>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
@@ -44,13 +45,16 @@
 
 <script>
 export default {
-  data: () => ({
-    email: '',
-    password: ''
-  }),
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   methods: {
     register() {
       /* eslint-disable no-console */
+      console.log(this.email, this.password)
       console.log('criando')
       this.axios
         .post('register/', {
@@ -70,7 +74,6 @@ export default {
           console.log('errou')
           console.log(error)
         })
-      //console.log(this.email, this.senha)
     }
   }
 }
